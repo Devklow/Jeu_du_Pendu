5a588e684d1e131086c7f8675b583f3c55c7bf7c
Devklow <wolkirf@gmail.com>
2021-10-26 13:13:20 +0200

    Ajoutez des fichiers projet.

---
+++ TestGraphique/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿
+namespace TestGraphique
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.tbxEntry = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.lblWTF = new System.Windows.Forms.Label();
+            this.lblWordToFind = new System.Windows.Forms.Label();
+            this.lblAlreadyGivenLetters = new System.Windows.Forms.Label();
+            this.lblAGL = new System.Windows.Forms.Label();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.pbxPendu = new System.Windows.Forms.PictureBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.lblGG = new System.Windows.Forms.Label();
+            this.btnMode = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPendu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(295, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(334, 55);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Jeu du Pendu";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tbxEntry
+            // 
+            this.tbxEntry.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.tbxEntry.BackColor = System.Drawing.SystemColors.Window;
+            this.tbxEntry.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.tbxEntry.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbxEntry.ForeColor = System.Drawing.Color.Black;
+            this.tbxEntry.HideSelection = false;
+            this.tbxEntry.ImeMode = System.Windows.Forms.ImeMode.Close;
+            this.tbxEntry.Location = new System.Drawing.Point(527, 263);
+            this.tbxEntry.MaxLength = 15;
+            this.tbxEntry.Name = "tbxEntry";
+            this.tbxEntry.ShortcutsEnabled = false;
+            this.tbxEntry.Size = new System.Drawing.Size(300, 38);
+            this.tbxEntry.TabIndex = 2;
+            this.tbxEntry.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbxEntry.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(521, 219);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(191, 32);
+            this.lblInfo.TabIndex = 3;
+            this.lblInfo.Text = "Entrez le mot :";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblWTF
+            // 
+            this.lblWTF.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblWTF.AutoSize = true;
+            this.lblWTF.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWTF.Location = new System.Drawing.Point(476, 123);
+            this.lblWTF.Name = "lblWTF";
+            this.lblWTF.Size = new System.Drawing.Size(411, 36);
+            this.lblWTF.TabIndex = 4;
+            this.lblWTF.Text = "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _";
+            this.lblWTF.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.lblWTF.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblWordToFind
+            // 
+            this.lblWordToFind.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblWordToFind.AutoSize = true;
+            this.lblWordToFind.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWordToFind.Location = new System.Drawing.Point(477, 83);
+            this.lblWordToFind.Name = "lblWordToFind";
+            this.lblWordToFind.Size = new System.Drawing.Size(173, 27);
+            this.lblWordToFind.TabIndex = 5;
+            this.lblWordToFind.Text = "Mot à trouver : ";
+            this.lblWordToFind.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblAlreadyGivenLetters
+            // 
+            this.lblAlreadyGivenLetters.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblAlreadyGivenLetters.AutoSize = true;
+            this.lblAlreadyGivenLetters.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlreadyGivenLetters.Location = new System.Drawing.Point(476, 387);
+            this.lblAlreadyGivenLetters.Name = "lblAlreadyGivenLetters";
+            this.lblAlreadyGivenLetters.Size = new System.Drawing.Size(286, 32);
+            this.lblAlreadyGivenLetters.TabIndex = 6;
+            this.lblAlreadyGivenLetters.Text = "Lettres déjà données :";
+            // 
+            // lblAGL
+            // 
+            this.lblAGL.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblAGL.AutoSize = true;
+            this.lblAGL.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAGL.Location = new System.Drawing.Point(478, 425);
+            this.lblAGL.Name = "lblAGL";
+            this.lblAGL.Size = new System.Drawing.Size(23, 24);
+            this.lblAGL.TabIndex = 7;
+            this.lblAGL.Text = "A";
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnValidate.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnValidate.Location = new System.Drawing.Point(610, 316);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(136, 38);
+            this.btnValidate.TabIndex = 8;
+            this.btnValidate.Text = "Valider";
+            this.btnValidate.UseVisualStyleBackColor = false;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
+            // 
+            // pbxPendu
+            // 
+            this.pbxPendu.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pbxPendu.Image = global::TestGraphique.Properties.Resources.dpendu0;
+            this.pbxPendu.Location = new System.Drawing.Point(-2, -1);
+            this.pbxPendu.Name = "pbxPendu";
+            this.pbxPendu.Size = new System.Drawing.Size(465, 513);
+            this.pbxPendu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxPendu.TabIndex = 0;
+            this.pbxPendu.TabStop = false;
+            this.pbxPendu.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnReset.Image = global::TestGraphique.Properties.Resources.dreset;
+            this.btnReset.Location = new System.Drawing.Point(801, 12);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(40, 40);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // lblGG
+            // 
+            this.lblGG.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblGG.AutoSize = true;
+            this.lblGG.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGG.Location = new System.Drawing.Point(528, 240);
+            this.lblGG.Name = "lblGG";
+            this.lblGG.Size = new System.Drawing.Size(299, 73);
+            this.lblGG.TabIndex = 10;
+            this.lblGG.Text = "GAGNÉ !";
+            // 
+            // btnMode
+            // 
+            this.btnMode.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnMode.Image = global::TestGraphique.Properties.Resources.lightmode;
+            this.btnMode.Location = new System.Drawing.Point(847, 12);
+            this.btnMode.Name = "btnMode";
+            this.btnMode.Size = new System.Drawing.Size(40, 40);
+            this.btnMode.TabIndex = 11;
+            this.btnMode.UseVisualStyleBackColor = false;
+            this.btnMode.Click += new System.EventHandler(this.btnMode_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(905, 515);
+            this.Controls.Add(this.btnMode);
+            this.Controls.Add(this.lblGG);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnValidate);
+            this.Controls.Add(this.lblAGL);
+            this.Controls.Add(this.lblAlreadyGivenLetters);
+            this.Controls.Add(this.lblWordToFind);
+            this.Controls.Add(this.lblWTF);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.tbxEntry);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.pbxPendu);
+            this.ForeColor = System.Drawing.Color.LightSalmon;
+            this.Name = "Form1";
+            this.Text = "Jeu du Pendu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPendu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox tbxEntry;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Label lblWordToFind;
+        private System.Windows.Forms.Label lblAlreadyGivenLetters;
+        private System.Windows.Forms.Label lblAGL;
+        private System.Windows.Forms.Label lblWTF;
+        private System.Windows.Forms.Button btnValidate;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblGG;
+        private System.Windows.Forms.PictureBox pbxPendu;
+        private System.Windows.Forms.Button btnMode;
+    }
+}
+
---
+++ TestGraphique/Form1.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestGraphique
+{
+    public partial class Form1 : Form
+    {
+        //public string[] alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R","S","T","U","V","W","X","Y","Z"];
+        public string alphabet1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public string alphabet2 = "abcdefghijklmnopqrstuvwxyz";
+        public bool Nightmode = true;
+        public int phase, viePerdu;
+        public string motChoisit, motAfficher, lettreChoisit, lettreDonnees, nmode;
+        public Form1()
+        {
+            InitializeComponent();
+            nmode = "d";
+            resetfunc();
+            AcceptButton = btnValidate;
+        }
+        /// <summary>
+        /// Permet de réinitialiser tout les valeurs et afficher la phase 1
+        /// </summary>
+        private void resetfunc()
+        {
+            viePerdu = 0;
+            Image monImage = (Image)Properties.Resources.ResourceManager.GetObject(nmode + "pendu" + viePerdu);
+            pbxPendu.Image = monImage;
+            lettreDonnees = "";
+            lblGG.Text = "";
+            lblGG.Visible = false;
+            phase = 1;
+            lblInfo.Text = "Entrez le mot :";
+            tbxEntry.Text = "";
+            tbxEntry.MaxLength = 15;
+            motAfficher = "";
+            lblWTF.Text = motAfficher;
+            lblWTF.Visible = false;
+            lblWordToFind.Visible = false;
+            lblAGL.Text = "";
+            lblAGL.Visible = false;
+            btnValidate.Visible = true;
+            tbxEntry.Visible = true;
+            lblInfo.Visible = true;
+            lblAlreadyGivenLetters.Visible = false;
+        }
+        /// <summary>
+        /// Permet d'iniatialiser la phase 2
+        /// </summary>
+        private void phaseInit()
+        {
+            phase = 2;
+            lblInfo.Text = "Entrez une lettre :";
+            tbxEntry.Text = "";
+            tbxEntry.MaxLength = 1;
+            int i = 0;
+            while (i < motChoisit.Length)
+            {
+                motAfficher = motAfficher + "_ ";
+                i += 1 ;
+            }
+            lblWTF.Text = motAfficher;
+            lblWTF.Visible = true;
+            lblWordToFind.Visible = true;
+            lblAGL.Text = "";
+            lblAGL.Visible = true;
+            lblAlreadyGivenLetters.Visible = true;
+        }
+        private void btnMode_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    /// <summary>
+    /// Permet de regarder si la lettre donnée correspond ou non à une lettre du mot
+    /// Elle change aussi l'image du pendu et fait perdre une vie
+    /// Elle actualise aussi l'affichage du mot a trouver et les lettres déjà essayée
+    /// </summary>
+    private void checkLetter()
+        {
+            int a, i;
+            string lemot = "";
+            a = motChoisit.IndexOf(lettreChoisit);
+            if(a > -1)
+            {
+                i = 0;
+                while (i < motChoisit.Length)
+                {
+                    if (motChoisit.ElementAt(i) == lettreChoisit.ElementAt(0))
+                    {
+                        lemot = lemot + lettreChoisit +" ";
+                    }
+                    else if(motAfficher.ElementAt(i*2) == '_')
+                    {
+                        lemot = lemot + "_ ";
+                    }
+                    else
+                    {
+                        lemot = lemot + motAfficher.ElementAt(i * 2) + " ";
+                    }
+                    i += 1;
+                }
+                motAfficher = lemot;
+            }
+            else
+            {
+                viePerdu += 1;
+                Image monImage = (Image)Properties.Resources.ResourceManager.GetObject(nmode+"pendu" + viePerdu);
+                pbxPendu.Image = monImage;
+            }
+            if(lettreDonnees.Length>0)
+            {
+                lettreDonnees = lettreDonnees + ", " + lettreChoisit;
+            }
+            else
+            {
+                lettreDonnees = lettreChoisit;
+
+            }
+            lblWTF.Text = motAfficher;
+            lblAGL.Text = lettreDonnees;
+
+        }
+        /// <summary>
+        /// Fonction permettant de passer du mode jour au mode nuit
+        /// change la couleur du fond, le couleur d'écriture et l'image du pendu
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnMode_Click_1(object sender, EventArgs e)
+        {
+            if (Nightmode)
+            {
+                Nightmode = false;
+                nmode = "";
+                Image monImage = (Image)Properties.Resources.ResourceManager.GetObject(nmode + "pendu" + viePerdu);
+                pbxPendu.Image = monImage;
+                monImage = (Image)Properties.Resources.ResourceManager.GetObject(nmode + "reset");
+                btnReset.Image = monImage;
+                monImage = (Image)Properties.Resources.ResourceManager.GetObject("nightmode");
+                btnMode.Image = monImage;
+                BackColor = Color.Black;
+                BackColor = Color.White;
+                ForeColor = Color.Black;
+            }
+            else
+            {
+                Nightmode = true;
+                nmode = "d";
+                Image monImage = (Image)Properties.Resources.ResourceManager.GetObject(nmode + "pendu" + viePerdu);
+                pbxPendu.Image = monImage;
+                monImage = (Image)Properties.Resources.ResourceManager.GetObject(nmode + "reset");
+                btnReset.Image = monImage;
+                monImage = (Image)Properties.Resources.ResourceManager.GetObject("lightmode");
+                btnMode.Image = monImage;
+                BackColor = Color.Black;
+                ForeColor = Color.LightSalmon;
+            }
+            
+
+        }
+
+        /// <summary>
+        /// Permet a chaque entrée de character de vérifier si celui ci est présent dans notre alphabet 
+        /// Si la lettre rentrée est une minuscule elle est remplacé par une majuscule
+        /// Si le caractère n'est pas compris dans l'alphabet celui ci n'est pas pris en compte
+        /// Pour la phase 2 si notre caractere appartient a l'alphabet mais a deja été proposé, il n'est pas pris en compte
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            char lettre = 'a';
+            int i = 0, a, b, c;
+            string letext = "";
+            while (i < tbxEntry.Text.Length)
+            {
+                try
+                {
+                    lettre = tbxEntry.Text[i];
+                }
+                catch { }
+                a = alphabet1.IndexOf(lettre);
+                b = alphabet2.IndexOf(lettre);
+                c = lettreDonnees.IndexOf(lettre);
+                if (a > -1 && c == -1)
+                {
+                    letext = letext + lettre;
+                }
+                if (b > -1 && c == -1)
+                {
+                    letext = letext + (char)alphabet1.ElementAt(b);
+                }
+                i += 1;
+            }
+            tbxEntry.Text = letext;
+        }
+        private void validate()
+        {
+            if (phase == 1 && tbxEntry.Text != "")
+            {
+                motChoisit = tbxEntry.Text;
+                phaseInit();
+            }
+            if (phase == 2 && tbxEntry.Text != "")
+            {
+                lettreChoisit = tbxEntry.Text;
+                tbxEntry.Text = "";
+                checkLetter();
+                checkWin();
+            }
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+            {
+                resetfunc();
+            }
+        /// <summary>
+        /// Permet de vérifier si le joueur a Gagner ou Perdu
+        /// </summary>
+        private void checkWin()
+        {
+            string motTrouver;
+            motTrouver = motAfficher.Replace(" ", String.Empty);
+            if (motTrouver == motChoisit)
+            {
+                lblGG.Text = "GAGNÉ !";
+                lblGG.Visible = true;
+                lblWTF.Text = motChoisit;
+                tbxEntry.Visible = false;
+                lblInfo.Visible = false;
+                btnValidate.Visible = false;
+            }
+            else if(viePerdu >= 10)
+            {
+                lblGG.Text = "PERDU !";
+                lblGG.Visible = true;
+                lblWTF.Text = motChoisit;
+                tbxEntry.Visible = false;
+                lblInfo.Visible = false;
+                btnValidate.Visible = false;
+            }
+        }
+        /// <summary>
+        /// Fonction du bouton valider qui appelle d'autre fonction en fonction de la phase
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            validate();
+        }
+    }
+}
